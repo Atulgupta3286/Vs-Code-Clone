cdf57a3d3ee9559276507285b53c465ecae81bf8
AtulGupta3286 <princegupta9302@gmail.com>
2023-11-22 01:42:41 +0530

    Sixth commit Day-29

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 var fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  fs.readdir("./uploads", function(err,files){
-    res.render("index", {files: files});
+router.get('/', function (req, res) {
+  fs.readdir("./uploads", { withFileTypes: true }, function (err, files) {
+    res.render("index", { files: files });
   })
 });
 
-router.get('/filecreate', function(req, res) {
-  fs.writeFile(`./uploads/${req.query.filename}`,"",function(err){
-    if(err) res.send(err);
+router.get('/filecreate', function (req, res) {
+  fs.writeFile(`./uploads/${req.query.filename}`, "", function (err) {
+    if (err) res.send(err);
     else res.redirect("back");
   });
 });
 
-router.get('/foldercreate', function(req, res) {
-  fs.mkdir(`./uploads/${req.query.foldername}`,function(err){
-    if(err) res.send(err);
+router.get('/foldercreate', function (req, res) {
+  fs.mkdir(`./uploads/${req.query.foldername}`, function (err) {
+    if (err) res.send(err);
     else res.redirect("back");
   });
 });

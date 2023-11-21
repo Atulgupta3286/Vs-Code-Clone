939db1dc7009766e1973102629ec024786b94126
AtulGupta3286 <princegupta9302@gmail.com>
2023-11-22 00:19:23 +0530

    Third commit Day-26

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index');
+});
+
+router.get('/filecreate', function(req, res) {
+  console.log(req.query);
 });
 
 module.exports = router;
